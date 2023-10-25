a10ceaf383dcd1271c099ebc95dbf7720a5c47db
Abdul Aziz <46093556+khanaaziz1@users.noreply.github.com>
2023-10-26 02:53:41 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const HomePage = () => (
+  <div className="App">
+    <h1 className="flex justify-center items-start w-full py-[10px] gap-x-[40px]">
+      Text
+    </h1>
+
+    <Button type="primary">Button</Button>
+  </div>
+)
+
+export default HomePage
